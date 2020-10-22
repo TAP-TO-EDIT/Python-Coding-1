ec689fa747dd3ec7cd3c5b26d4672e72ff0fb4ef
TAP-TO-EDIT <69836843+TAP-TO-EDIT@users.noreply.github.com>
2020-10-22 13:35:28 +0800

    content should be improved

--- encouragement.py
+++ encouragement.py
@@ -1,7 +1,7 @@
-print("Title of program: Encouragement bot")
+print("Encouragement bot thingy")
 print()
 while True:
-  description = input("Could you describe how you feel in a sentence?")
+  description = input("Could you describe how you feel in one word?  ")
 
   list_of_words = description.split()
 
@@ -11,21 +11,49 @@ while True:
   
   for each_word in list_of_words:
     
-    if each_word == "excited":
-      feelings_list.append("excited")
-      encouragement_list.append("I see that you are excited. That's cool!")
-      counter += 1
-    if each_word == "tired":
-      feelings_list.append("tired")
-      encouragement_list.append("It doesn't mean that its over. Take a rest and put yourself back on a slingshot. You can do it!")
-      counter += 1
-    if each_word == "depressed":
-      feelings_list.append("depressed")
-      encouragement_list.append("Do you need someone to talk to? I may be a bot but I'll be here!")
+    if each_word == "sad":
+      feelings_list.append("sad")
+      encouragement_list.append("there is a small chance that tomorrow will be a better day")
       counter += 1
     if each_word == "happy":
       feelings_list.append("happy")
-      encouragement_list.append("Your smile is beautiful. Keep smiling! It brightens up my day!")
+      encouragement_list.append("hapiness is temporary")
+      counter += 1
+    if each_word == "tired":
+      feelings_list.append("tired")
+      encouragement_list.append("go and sleep")
+      counter += 1
+    if each_word == "useless":
+      feelings_list.append("useless")
+      encouragement_list.append("you are more useful than annoying ads that companies spend thousands on")
+      counter += 1
+    if each_word == "shitty":
+      feelings_list.append("shitty")
+      encouragement_list.append("we all feel like shit")
+      counter += 1
+    if each_word == "stressed":
+      feelings_list.append("stressed")
+      encouragement_list.append("if you are stressed, then too bad for you")
+      counter += 1
+    if each_word == "great":
+      feelings_list.append("great")
+      encouragement_list.append("you will still die, someday")
+      counter += 1
+    if each_word == "good":
+      feelings_list.append("good")
+      encouragement_list.append("there are millions of children dying of starvation in Africa")
+      counter += 1
+    if each_word == "terrible":
+      feelings_list.append("terrible")
+      encouragement_list.append("the millions of children dying of starvation in Africa are feeling more terrible than you")
+      counter += 1
+    if each_word == "genocide":
+      feelings_list.append("like starting a genocide")
+      encouragement_list.append("people generally don't like that")
+      counter += 1
+    if each_word == "crappy":
+      feelings_list.append("crappy")
+      encouragement_list.append("your country has a sewerage system, so use it")
       counter += 1
 
 
@@ -54,3 +82,4 @@ while True:
   print()
   print(output)
   print()
+
